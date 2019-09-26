e083a974c389c5bda8c8308c7c92e7f91481efdd
Lucas PenzeyMoog <lucaspenzeymoog@gmail.com>
2019-09-26 16:50:49 -0500

    Add version two routing

---
+++ src/main/scala/com/lpenzey/routes/v2/UserRoutesv2.scala
@@ -0,0 +1,104 @@
+package com.lpenzey.routes.v2
+
+import akka.actor.{ActorRef, ActorSystem}
+import akka.event.Logging
+
+import scala.concurrent.duration._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.directives.MethodDirectives.{delete, get, post}
+import akka.http.scaladsl.server.directives.PathDirectives.path
+import akka.http.scaladsl.server.directives.RouteDirectives.complete
+import scala.concurrent.Future
+
+import com.lpenzey.actors.RegisterUser._
+import akka.pattern.ask
+import akka.util.Timeout
+import com.lpenzey.dao.UsersDao
+import com.lpenzey.models.{User, Users}
+import com.lpenzey.helpers.{AuthenticateBasicAsync, CORSHandler, ExceptionHelper, JsonSupport}
+
+trait UserRoutesv2
+  extends JsonSupport
+    with AuthenticateBasicAsync
+    with ExceptionHelper
+{
+  lazy val usersV2Log = Logging(system, classOf[UserRoutesv2])
+
+  implicit def system: ActorSystem
+
+  def registerUser: ActorRef
+
+  implicit lazy val timeout: Timeout = Timeout(10.seconds)
+
+  private val cors = new CORSHandler {}
+
+  def userRoutesv2: Route = pathPrefix("v2") {
+    pathPrefix("users") {
+      pathPrefix("register") {
+        pathEnd {
+          concat(
+            get {
+              val users: Future[Users] =
+                (registerUser ? GetUsers).mapTo[Users]
+              cors.corsHandler(complete(users))
+            },
+            options {
+              cors.corsHandler(complete(StatusCodes.OK))
+            },
+            post {
+              entity(as[User]) { user =>
+                val userCreated: Future[ActionPerformed] = (registerUser ? CreateUser(user)).mapTo[ActionPerformed]
+                onSuccess(userCreated) { performed =>
+                  usersV2Log.info(s"Created User [${user.name}]: ${performed.action}")
+                  cors.corsHandler(complete(StatusCodes.Created, performed))
+                }
+              }
+            })
+        } ~
+          path(Segment) { name =>
+            get {
+              val maybeUser: Future[User] = (registerUser ? GetUser(name)).mapTo[User]
+              rejectEmptyResponse {
+                {
+                  cors.corsHandler(complete(maybeUser))
+                }
+              }
+            }
+          }
+      } ~
+        delete {
+          optionalHeaderValue(extractAuthHeader) { token =>
+            val myToken = token.getOrElse("not a token").toString
+            val userDeleted: Future[ActionPerformed] = (registerUser ? DeleteUser(myToken)).mapTo[ActionPerformed]
+            onSuccess(userDeleted) { performed =>
+              usersV2Log.info(s"Deleted user", performed.action)
+              cors.corsHandler(complete(StatusCodes.OK, performed))
+            }
+          }
+        } ~
+        pathPrefix("login") {
+          pathEnd {
+            concat(
+              options {
+                cors.corsHandler(complete(StatusCodes.OK))
+              },
+              post {
+                authenticateBasicAsync("", UsersDao.authUser) { user =>
+                  val token: Future[Any] = registerUser ? CreateToken(user)
+                  onSuccess(token) { token =>
+                    val authHeader = RawHeader("Authorization", "Bearer " + token.toString)
+                    cors.corsHandler(respondWithHeader(authHeader) {
+                      complete(StatusCodes.OK)
+                    })
+                  }
+                }
+              }
+            )
+          }
+        }
+    }
+  }
+}
